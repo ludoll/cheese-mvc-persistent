5caa1b1dffafe008edfe2bcebafaa3557464395c
luke.doll <31712880+ludoll@users.noreply.github.com>
2017-12-19 18:34:29 -0600

    Final Commit

--- src/main/java/org/launchcode/models/Cheese.java
+++ src/main/java/org/launchcode/models/Cheese.java
@@ -1,10 +1,9 @@
 package org.launchcode.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 /**
  * Created by LaunchCode
@@ -17,25 +16,29 @@ public class Cheese {
     private int id;
 
     @NotNull
-    @Size(min=3, max=15)
+    @Size(min=3, max=35, message = "Name is a required field")
     private String name;
 
     @NotNull
     @Size(min=1, message = "Description must not be empty")
     private String description;
 
-    private CheeseType type;
+    @ManyToOne
+    private Category category;
+
+    @ManyToMany(mappedBy = "cheeses")
+    private List<Menu> menus;
+
 
     public Cheese(String name, String description) {
         this.name = name;
         this.description = description;
     }
 
-    public Cheese() { }
 
-    public int getId() {
-        return id;
-    }
+    public Cheese() {  }
+
+    public int getId() { return id; }
 
     public String getName() {
         return name;
@@ -53,11 +56,33 @@ public class Cheese {
         this.description = description;
     }
 
-    public CheeseType getType() {
-        return type;
+    public Category getCategory() { return this.category; }
+
+    public void setCategory(Category category) {
+        this.category = category;
     }
 
-    public void setType(CheeseType type) {
-        this.type = type;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Cheese)) return false;
+
+        Cheese cheese = (Cheese) o;
+
+        if (id != cheese.id) return false;
+        if (name != null ? !name.equals(cheese.name) : cheese.name != null) return false;
+        if (description != null ? !description.equals(cheese.description) : cheese.description != null) return false;
+        if (category != null ? !category.equals(cheese.category) : cheese.category != null) return false;
+        return menus != null ? menus.equals(cheese.menus) : cheese.menus == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (category != null ? category.hashCode() : 0);
+        result = 31 * result + (menus != null ? menus.hashCode() : 0);
+        return result;
     }
 }
